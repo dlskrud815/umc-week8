bf48f0795a71d4c366cdd8950631c6cd3c02d378
dlskrud815 <dlskrud815@gmail.com>
2023-11-21 00:50:00 +0900

    8주차 실습

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [city, setCity] = useState("");
+  const [weatherData, setWeatherData] = useState(null);
+
+  const API_KEY = "c200a8fcfcfad0023da3960702bcfa85";
+
+  const searchWeather = async () => {
+    try {
+      const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`;
+      const response = await axios.get(apiUrl);
+      setWeatherData(response.data);
+    } catch (error) {
+      console.error("Error fetching data: ", error);
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setCity(event.target.value);
+  };
+
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      searchWeather();
+    }
+  };
+
+  const convertToCelsius = (temp) => {
+    if (typeof temp === "number") {
+      return `${(temp - 273.15).toFixed(2)}°C`;
+    }
+    return "Invalid temperature";
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="weather-container">
+        <input
+          className="weather-input"
+          type="text"
+          placeholder="도시를 입력해주세요."
+          value={city}
+          onChange={handleInputChange}
+          onKeyPress={handleKeyPress}
+        />
+        {weatherData && (
+          <div className="weather-info">
+            <p className="city-name">{weatherData.name}</p>
+            <p className="city-temp">
+              {convertToCelsius(weatherData.main.temp)}
+            </p>
+            <p className="city-weather">{weatherData.weather[0].description}</p>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
